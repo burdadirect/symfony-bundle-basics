e4ed718ba2a961f35ab1db1ee386a853673ff5a7
Christian Puchinger <puchinger@burda.com>
2020-03-11 15:47:39 +0100

    Add mode for implicit login event.

--- Security/Events/ImplicitLoginEvent.php
+++ Security/Events/ImplicitLoginEvent.php
@@ -10,4 +10,35 @@ use \Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
  * This class is to determine an implicit (programatically) login event.
  */
 class ImplicitLoginEvent extends InteractiveLoginEvent  {
+
+  public const MODE_EMAIL_CONFIRMATION = 'email_confirmation';
+  public const MODE_PASSWORD_RESET = 'password_reset';
+
+  /**
+   * @var string
+   */
+  private $mode;
+
+  /**
+   * Set mode.
+   *
+   * @param string $mode
+   *
+   * @return self
+   */
+  public function setMode(string $mode = NULL) : self {
+    $this->mode = $mode;
+
+    return $this;
+  }
+
+  /**
+   * Get mode.
+   *
+   * @return string|null
+   */
+  public function getMode() : ?string {
+    return $this->mode;
+  }
+
 }

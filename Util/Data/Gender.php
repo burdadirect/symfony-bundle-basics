25ced4e6d56a0564313c37320b78cf870e95aa96
Christian Puchinger <puchinger@burda.com>
2021-06-15 04:42:57 +0200

    Update Gender.php

--- Util/Data/Gender.php
+++ Util/Data/Gender.php
@@ -51,13 +51,13 @@ class Gender extends AbstractData {
 
   /**
    * Determine gender from various strings.
-   * 
+   *
    * @param string|null $gender
    * @return string|null
    */
   public static function determine(?string $gender) : ?string {
     if ($gender) {
-      $gender = strtolower($gender);
+      $gender = mb_strtolower($gender);
 
       foreach (self::data() as $key => $value) {
         if (in_array($gender, $value['aliases'], TRUE)) {

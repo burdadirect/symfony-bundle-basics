fb94fe52e17666210bc1b0fb8649c8e85bf6f232
Christian Puchinger <puchinger@burda.com>
2022-01-27 03:33:00 +0100

    Update ImplicitLoginEvent.php

--- Security/Events/ImplicitLoginEvent.php
+++ Security/Events/ImplicitLoginEvent.php
@@ -2,14 +2,14 @@
 
 namespace HBM\BasicsBundle\Security\Events;
 
-use \Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
 
 /**
  * Class ImplicitLoginEvent
  *
  * This class is to determine an implicit (programatically) login event.
  */
-class ImplicitLoginEvent extends InteractiveLoginEvent  {
+class ImplicitLoginEvent extends LoginSuccessEvent {
 
   public const MODE_EMAIL_CONFIRMATION = 'email_confirmation';
   public const MODE_PASSWORD_RESET = 'password_reset';

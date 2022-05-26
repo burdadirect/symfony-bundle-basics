5d036f2880f54b83f2536b66138c7f882a7b0949
Christian Puchinger <puchinger@burda.com>
2022-05-26 17:39:54 +0200

    Create FormFactoryDependencyTrait.php

---
+++ Traits/ServiceDependencies/FormFactoryDependencyTrait.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace HBM\BasicsBundle\Traits\ServiceDependencies;
+
+use Symfony\Component\Form\FormFactoryInterface;
+
+trait FormFactoryDependencyTrait {
+
+  protected FormFactoryInterface $formFactory;
+
+  /**
+   * @required
+   *
+   * @param FormFactoryInterface $formFactory
+   *
+   * @return void
+   */
+  public function setFormFactory(FormFactoryInterface $formFactory): void {
+    $this->pb = $formFactory;
+  }
+
+}

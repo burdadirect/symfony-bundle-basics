1af163304bca1a7351a1b9d3b1f8f76fdf93dd80
Joachim Krempel <joachim.krempel@burda.com>
2023-08-24 16:30:00 +0200

    Fix Extension::load signature

--- src/DependencyInjection/HBMBasicsExtension.php
+++ src/DependencyInjection/HBMBasicsExtension.php
@@ -19,7 +19,7 @@ class HBMBasicsExtension extends Extension {
    *
    * @throws \Exception
    */
-  public function load(array $configs, ContainerBuilder $container) {
+  public function load(array $configs, ContainerBuilder $container): void {
     $configuration = new Configuration();
     $config = $this->processConfiguration($configuration, $configs);
 

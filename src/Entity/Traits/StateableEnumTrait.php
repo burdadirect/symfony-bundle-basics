31d4869dfa6d35f4bc8af3190c703c147d6ac56a
Christian Puchinger <puchinger@burda.com>
2024-09-05 17:26:33 +0200

    Add enum state interface and trait.

---
+++ src/Entity/Traits/StateableEnumTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace HBM\BasicsBundle\Entity\Traits;
+
+use HBM\BasicsBundle\Util\Data\State;
+use HBM\BasicsBundle\Util\Enum\Interfaces\EnumStateInterface;
+
+/**
+ * @template TT
+ */
+trait StateableEnumTrait
+{
+    /**
+     * Set state.
+     *
+     * @param TT $state
+     */
+    public function setState(EnumStateInterface $state)
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    /**
+     * Get state.
+     *
+     * @return TT
+     */
+    public function getState(): EnumStateInterface
+    {
+        return $this->state;
+    }
+}
---
+++ src/Util/Enum/Interfaces/StateInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace HBM\BasicsBundle\Util\Enum\Interfaces;
+
+interface EnumStateInterface
+{
+}

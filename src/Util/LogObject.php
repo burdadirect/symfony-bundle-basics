58b00c3875c796d43c6b1d6612e42ba2eeb1cd91
Christian Puchinger <puchinger@burda.com>
2025-10-19 21:17:16 +0200

    Update LogObject.php

--- src/Util/LogObject.php
+++ src/Util/LogObject.php
@@ -3,13 +3,15 @@
 namespace HBM\BasicsBundle\Util;
 
 use HBM\BasicsBundle\Entity\AbstractEntity;
+use HBM\BasicsBundle\Util\Result\Result;
 
 class LogObject
 {
     public function __construct(
         private ?string $message = null,
         private array $contexts = [],
-        private array $entities = []
+        private array $entities = [],
+        private ?Result $result = null
     ) {
     }
 
@@ -70,6 +72,18 @@ class LogObject
         return $this;
     }
 
+    public function getResult(): ?Result
+    {
+        return $this->result;
+    }
+
+    public function setResult(?Result $result): LogObject
+    {
+        $this->result = $result;
+
+        return $this;
+    }
+
     public function pinpoint(string $methodOrFunction, string|int|null $line = null): LogObject
     {
         return $this->addContext('pinpoint', $methodOrFunction . ($line !== null ? '[' . $line . ']' : ''));
@@ -86,18 +100,26 @@ class LogObject
             }
         }
 
-        if (count($this->entities) > 0) {
+        if (count($this->getEntities()) > 0) {
             $context['entities'] = array_map(static function (AbstractEntity $entity) {
                 return $entity->getIdFQCN();
             }, $this->getEntities());
         }
 
-        return [strip_tags($this->getMessage()), $context];
+        if ($this->getResult() !== null) {
+            foreach ($this->getResult()->getMessages() as $message) {
+                $context[] = $message;
+            }
+        }
+
+        $message = $this->getMessage() ? strip_tags($this->getMessage()) : null;
+
+        return [$message, $context];
     }
 
-    public static function arguments(?string $message = null, array $contexts = [], array $entities = []): array
+    public static function arguments(?string $message = null, array $contexts = [], array $entities = [], ?Result $result = null): array
     {
-        return (new LogObject($message, $contexts, $entities))->toLoggerArgumentsArray();
+        return (new LogObject($message, $contexts, $entities, $result))->toLoggerArgumentsArray();
     }
 
     public function copy(): LogObject

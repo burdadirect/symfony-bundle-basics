b3ecdc54c1aaef05575f7ec4d65f67abf0676da9
Christian Puchinger <puchinger@burda.com>
2025-10-24 00:48:47 +0200

    Update StringToArrayTransformer.php

--- src/Form/DataTransformer/StringToArrayTransformer.php
+++ src/Form/DataTransformer/StringToArrayTransformer.php
@@ -6,32 +6,20 @@ use Symfony\Component\Form\DataTransformerInterface;
 
 class StringToArrayTransformer implements DataTransformerInterface
 {
-    /** @var string */
-    protected $separator;
+    protected string $separator;
+    protected string $glue;
 
-    /** @var string */
-    protected $glue;
-
-    /**
-     * StringToArrayTransformer constructor.
-     */
-    public function __construct(string $separator, string $glue = null)
+    public function __construct(string $separator, ?string $glue = null)
     {
         $this->separator = $separator;
         $this->glue      = $glue ?: $separator;
     }
 
-    /**
-     * @return mixed|string
-     */
     public function transform($value)
     {
         return implode($this->glue, $value);
     }
 
-    /**
-     * @return array|mixed
-     */
     public function reverseTransform($value)
     {
         return array_map('trim', explode($this->separator, $value));

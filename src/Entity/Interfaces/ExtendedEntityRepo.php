247e81aeedc0b0949dc03eaa2457b15fa9a08e47
Christian Puchinger <puchinger@burda.com>
2025-11-03 17:39:19 +0100

    CS fixer.

--- src/Entity/Interfaces/ExtendedEntityRepo.php
+++ src/Entity/Interfaces/ExtendedEntityRepo.php
@@ -9,46 +9,19 @@ interface ExtendedEntityRepo extends ObjectRepository
 {
     public function createQueryBuilderForAlias(string $alias);
 
-    /**
-     * Finds entities by a set of criteria.
-     *
-     * @param null|int $limit
-     *
-     * @return array the objects
-     */
-    public function findRandomBy(array $criteria, $limit = null): array;
+    public function findRandomBy(array $criteria, ?int $limit = null): array;
 
     public function addSortations(QueryBuilder $qb, array $sortations, array $default = []): QueryBuilder;
 
-    /**
-     * Adjust a query builder for searching.
-     *
-     * @param null|int|mixed|string $value
-     */
-    public function searchValue(QueryBuilder $qb, string $alias, string $field, $value = null, string $prefix = 'value'): QueryBuilder;
+    public function searchValue(QueryBuilder $qb, string $alias, string $field, mixed $value = null, string $prefix = 'value'): QueryBuilder;
 
-    /**
-     * Adjust a query builder for searching.
-     */
-    public function searchChoices(QueryBuilder $qb, string $alias, string $field, array $choices = null, string $prefix = 'choices'): QueryBuilder;
+    public function searchChoices(QueryBuilder $qb, string $alias, string $field, ?array $choices = null, string $prefix = 'choices'): QueryBuilder;
 
-    /**
-     * Adjust a query builder for searching.
-     */
-    public function searchSelection(QueryBuilder $qb, string $alias, string $field, array $selections = null, string $prefix = 'selections'): QueryBuilder;
+    public function searchSelection(QueryBuilder $qb, string $alias, string $field, ?array $selections = null, string $prefix = 'selections'): QueryBuilder;
 
-    /**
-     * Adjust a query builder for searching.
-     */
-    public function searchNull(QueryBuilder $qb, string $alias, string $field, bool $isNull = null): QueryBuilder;
+    public function searchNull(QueryBuilder $qb, string $alias, string $field, ?bool $isNull = null): QueryBuilder;
 
-    /**
-     * Add search field to querybuilder.
-     */
     public function addSearchFields(QueryBuilder $qb, array $fields, array $words, string $prefix = 'search', string $format = '%%%s%%', string $method = 'like'): QueryBuilder;
 
-    /**
-     * Check if querybuilder already has been joined.
-     */
     public function leftJoinOnce(QueryBuilder $qb, string $alias, string $field, string $joinAlias): QueryBuilder;
 }
